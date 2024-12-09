699a10731c6891ecb0443b847ee02d317c867dd5
SebaPrado <sebastian.pradomelesi@gmail.com>
2024-12-09 13:38:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,17 +6,16 @@ const routes = require("./routes");
 const APP_PORT = process.env.APP_PORT || 3001;
 const app = express();
 
+// const favicon = require("serve-favicon");
+// const path = require("path");
 const cors = require("cors");
-const favicon = require("serve-favicon");
-const path = require("path");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.use(cors());
-
 
 routes(app);
 
